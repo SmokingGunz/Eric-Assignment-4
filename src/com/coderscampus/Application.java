c777bf7a8505346abe98dbd9ebe82343811a251f
SmokingGunz <ericowens08@gmail.com>
2023-02-26 08:57:42 -0800

    v2

--- src/com/coderscampus/Application.java
+++ src/com/coderscampus/Application.java
@@ -5,7 +5,7 @@ import java.io.IOException;
 public class Application {
 
 	public static void main(String[] args) throws IOException {
-		
+
 		FileService fileService = new FileService();
 		Student[] masterList = fileService.readStudents("student-master-list.csv");
 		StudentService.writeStudents(masterList);
