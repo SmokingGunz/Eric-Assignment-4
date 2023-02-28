644cc554861c3de9bba8927505aad1ee95878b72
SmokingGunz <ericowens08@gmail.com>
2023-02-27 21:27:10 -0800

    v4
    Cleaned up formatting.

--- src/com/coderscampus/StudentService.java
+++ src/com/coderscampus/StudentService.java
@@ -13,48 +13,47 @@ public class StudentService {
 		Student[] statStudents = new Student[33];
 		Student[] apmthStudents = new Student[33];
 
-			try (BufferedWriter compSciWriter = new BufferedWriter(new FileWriter("course1.csv"));
-					BufferedWriter statWriter = new BufferedWriter(new FileWriter("course2.csv"));
-					BufferedWriter apmthWriter = new BufferedWriter(new FileWriter("course3.csv"))) {
-				int compSci = 0;
-				int stat = 0;
-				int apmth = 0;
+		try (BufferedWriter compSciWriter = new BufferedWriter(new FileWriter("course1.csv"));
+				BufferedWriter statWriter = new BufferedWriter(new FileWriter("course2.csv"));
+				BufferedWriter apmthWriter = new BufferedWriter(new FileWriter("course3.csv"))) {
 
-				for (Student student : studentList) {
-					if (student.getCourse().contains("COMPSCI")) {
-						compSciStudents[compSci] = student;
-						compSci++;
-					}
-					if (student.getCourse().contains("STAT")) {
-						statStudents[stat] = student;
-						stat++;
-					}
-					if (student.getCourse().contains("APMTH")) {
-						apmthStudents[apmth] = student;
-						apmth++;
-					}
+			int compSci = 0;
+			int stat = 0;
+			int apmth = 0;
+
+			for (Student student : studentList) {
+				if (student.getCourse().contains("COMPSCI")) {
+					compSciStudents[compSci] = student;
+					compSci++;
 				}
-				Arrays.sort(compSciStudents);
-				compSciWriter.write("Student ID,Student Name,Course,Grade \n");
-				for (Student student : compSciStudents) {
-					compSciWriter.write(student.getStudentID() + "," + student.getStudentName() + "," + student.getCourse()
-							+ "," + student.getGrade() + "\n");
+				if (student.getCourse().contains("STAT")) {
+					statStudents[stat] = student;
+					stat++;
 				}
-				Arrays.sort(statStudents);
-				statWriter.write("Student ID,Student Name,Course,Grade \n");
-				for (Student student : statStudents) {
-					statWriter.write(student.getStudentID() + "," + student.getStudentName() + "," + student.getCourse()
-							+ "," + student.getGrade() + "\n");
-				}
-				Arrays.sort(apmthStudents);
-				apmthWriter.write("Student ID,Student Name,Course,Grade \n");
-				for (Student student : apmthStudents) {
-					apmthWriter.write(student.getStudentID() + "," + student.getStudentName() + "," + student.getCourse()
-							+ "," + student.getGrade() + "\n");
+				if (student.getCourse().contains("APMTH")) {
+					apmthStudents[apmth] = student;
+					apmth++;
 				}
 			}
-
-		
+			Arrays.sort(compSciStudents);
+			compSciWriter.write("Student ID,Student Name,Course,Grade \n");
+			for (Student student : compSciStudents) {
+				compSciWriter.write(student.getStudentID() + "," + student.getStudentName() + "," + student.getCourse()
+						+ "," + student.getGrade() + "\n");
+			}
+			Arrays.sort(statStudents);
+			statWriter.write("Student ID,Student Name,Course,Grade \n");
+			for (Student student : statStudents) {
+				statWriter.write(student.getStudentID() + "," + student.getStudentName() + "," + student.getCourse()
+						+ "," + student.getGrade() + "\n");
+			}
+			Arrays.sort(apmthStudents);
+			apmthWriter.write("Student ID,Student Name,Course,Grade \n");
+			for (Student student : apmthStudents) {
+				apmthWriter.write(student.getStudentID() + "," + student.getStudentName() + "," + student.getCourse()
+						+ "," + student.getGrade() + "\n");
+			}
+		}
 
 	}
 
